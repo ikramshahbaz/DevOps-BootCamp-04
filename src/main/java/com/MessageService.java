f902d4f9fe67f1ab83aed4a3c0e558e5e946a9ad
ikramshahbaz <ikramshahbaz@gmail.com>
2020-04-30 15:29:58 +0530

    first commit

---
+++ src/main/java/com/MessageService.java
@@ -0,0 +1,44 @@
+package com;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class MessageService{
+
+	
+	@GetMapping("/git")
+	public String soft_git() {
+
+		return "Hello git users";
+
+	}
+
+	@GetMapping("/maven")
+	public String soft_maven() {
+
+		return "Hello Maven Users!!!";
+
+	}
+	
+	@GetMapping("/eclipse")
+	public String soft_eclipse() {
+
+		return "Hello eclipse users";
+
+	}
+	
+	@GetMapping("/jenkins")
+	public String soft_jenkins() {
+
+		return "Hello jenkins Users!!!";
+
+	}
+	
+
+}
